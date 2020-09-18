f5cf0115e53c92f4b1132c3467ccae018c4595bb
Tyler Myracle <tyler.myracle@gmail.com>
2020-09-17 23:17:26 -0700

    scaffold dashboard page

--- client/src/pages/Dashboard.js
+++ client/src/pages/Dashboard.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { Button, Icon } from "semantic-ui-react";
+import { Link } from "react-router-dom";
 import { useAuth } from "../lib/authHandler";
 
 const Dashboard = () => {
@@ -9,8 +11,18 @@ const Dashboard = () => {
   }
 
   return (
-    <div>
-      Welcome to the dashboard {user.first_name} {user.last_name}!
+    <div className="p-6 bg-white rounded-lg max-w-lg mx-auto mt-24">
+      <div className="profile-img-placeholder text-center">
+        <Icon name="user" style={{ borderColor: "white", opacity: 1 }} />
+      </div>
+      <div className="text-center text-2xl font-bold mb-6">
+        Welcome, {user.first_name}!
+      </div>
+      <div className="text-center">
+        <Button as={Link} to="/profile" color="black">
+          Manage Profile
+        </Button>
+      </div>
     </div>
   );
 };
