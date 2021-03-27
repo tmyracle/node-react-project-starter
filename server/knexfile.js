1db8f36f563fa869af1f4bd96bf25d41f75e3b74
Tyler Myracle <tyler.myracle@gmail.com>
2021-03-26 23:05:01 -0700

    test

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,7 +1,7 @@
 const path = require("path");
 process.env.NODE_ENV !== "production"
   ? require("dotenv").config({ path: path.resolve(__dirname, ".env") })
-  : null;
+  : require("dotenv").config();
 
 module.exports = {
   development: {
