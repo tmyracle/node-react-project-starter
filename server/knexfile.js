dee7cb42d6f4a997074d4a467bad15d57cd7975a
Tyler Myracle <tyler.myracle@gmail.com>
2021-03-27 00:09:02 -0700

    plz fix

--- server/knexfile.js
+++ server/knexfile.js
@@ -35,7 +35,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL + "?sslmode=require",
     migrations: {
       directory: "./db/migrations",
     },
