eb4bde2d4304a986240c9a576522c51a26c91cd7
Tyler Myracle <tyler.myracle@gmail.com>
2021-03-26 23:53:44 -0700

    ehh

--- server/knexfile.js
+++ server/knexfile.js
@@ -35,10 +35,7 @@ module.exports = {
   },
   production: {
     client: "pg",
-    connection: {
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
-    },
+    connection: process.env.DATABASE_URL,
     migrations: {
       directory: "./db/migrations",
     },
