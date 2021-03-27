d4bb0bde492bf36e44c8bd1002cb47322b4114bf
Tyler Myracle <tyler.myracle@gmail.com>
2021-03-26 21:24:45 -0700

    fix

--- server/knexfile.js
+++ server/knexfile.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+const path = require("path");
+require("dotenv").config({ path: path.resolve(__dirname, ".env") });
 
 module.exports = {
   development: {
@@ -20,6 +21,20 @@ module.exports = {
     connection: {
       // TODO: update postgres container to create test db on start
       database: process.env.POSTGRES_TEST_DB,
+      user: process.env.POSTGRES_USER,
+      password: process.env.POSTGRES_PASSWORD,
+    },
+    migrations: {
+      directory: "./db/migrations",
+    },
+    seeds: {
+      directory: "./db/seeds",
+    },
+  },
+  production: {
+    client: "pg",
+    connection: {
+      database: process.env.POSTGRES_DB,
       user: process.env.POSTGRES_USER,
       password: process.env.POSTGRES_PASSWORD,
     },
